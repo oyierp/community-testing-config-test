9c4d1e53495343bb03ba2ff2374b0489c953dda7
Philip Oyier <45464990+oyierp@users.noreply.github.com>
2022-09-29 13:09:30 +0300

    Update contact-summary.templated.js

--- contact-summary.templated.js
+++ contact-summary.templated.js
@@ -5,16 +5,6 @@ const fields = [
   { appliesToType: 'person', label: 'contact.parent', value: lineage, filter: 'lineage' },
   { appliesToType: 'person', label: 'ID Number', value: contact.id_number || contact.passport_number || contact.alien_number || 'N/A', width: 4 },
 
-  { appliesToType: '!person', appliesIf: () => { return contact.contact_type === 'trace_case'; }, label: 'Case ID', value: contact && contact.case_id, width: 4 },
-  { appliesToType: '!person', appliesIf: () => { return /case|trace/.test(contact.contact_type); }, label: 'contact.age', value: contact.date_of_birth, width: 4, filter: 'age' },
-  { appliesToType: '!person', appliesIf: () => { return /case|trace/.test(contact.contact_type); }, label: 'contact.sex', value: 'contact.sex.' + contact.gender, translate: true, width: 4 },
-  { appliesToType: '!person', appliesIf: () => { return /case|trace/.test(contact.contact_type); }, label: 'contact.phone', value: contact && contact.phone_number, width: 4, filter: 'phone' },
-  { appliesToType: '!person', appliesIf: () => { return /case|trace/.test(contact.contact_type); }, label: 'ID Number', value: contact.id_number || contact.passport_number || contact.alien_number || 'N/A', width: 4 },
-  { appliesToType: '!person', appliesIf: () => { return contact.contact_type === 'trace_contact' && contact.transitioned_to_case; }, label: 'Transitioned to Case?', value: `Yes <a href='/#/contacts/${contact.transitioned_to_case}'><i class="fa fa-external-link" aria-hidden="true"></i> view more</a>`, width: 4, filter: 'safeHtml' },
-
-  { appliesToType: '!person', appliesIf: () => { return contact.contact_type === 'forwarded_case'; }, label: 'Subcounty', value: contact.subcounty || 'N/A' },
-  { appliesToType: '!person', appliesIf: () => { return contact.contact_type === 'forwarded_case'; }, label: 'Ward?', value: contact.ward || 'N/A' },
-  { appliesToType: '!person', appliesIf: () => { return contact.contact_type === 'forwarded_case'; }, label: 'Assigned to tracer?', value: !contact.assignee ? 'No' : 'Yes' },
 ];
 
 const now = new Date();
